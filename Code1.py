0fddb876711ee237cf14f0e02fc074a58a3e2a4d
Pablo Mac Kay <p372063@uabc.edu.mx>
2025-03-18 09:20:16 -0700

    Primeros 4 archivos

---
+++ Code1.py
@@ -0,0 +1,11 @@
+import random
+
+#number=random.randint(1,1000)
+number=random.randint(1,500)
+
+while number!=69:
+    print("Around the world")
+    #print("¿Quieres tacos de guaguacoa?")
+    #number=random.randint(1,1000)
+    number=random.randint(1,500)
+
---
+++ HelloWorld.py
@@ -0,0 +1,1 @@
+print("I made the iMac, I made the iPhone")
